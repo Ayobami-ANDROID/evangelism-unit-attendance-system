6dd8b21fcdd1fd62aff06abe53aa8ab8314cabe0
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2022-12-26 18:25:48 +0100

    security

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const app = express();
 const connectDB = require('./connectDB/connectDb')
 require('dotenv').config()
 const user = require('./routes/user')
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+app.set('trust proxy',1)
+
+app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(rateLimiter({
+  windowMs:15*60*1000,
+  max:100,
+}))
+
+app.get('/',(req,res) => {
+    res.send('job api')
+  })
 
 app.use(express.json())
 
